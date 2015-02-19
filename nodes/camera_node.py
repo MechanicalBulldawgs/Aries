9aa779b2de25c3b32e137606f9114a426f1e7b0b
Alex Lalejini <amlalejini@gmail.com>
2015-02-19 01:25:19 -0600

    Wrote webcam ROS node.  TODO: make cam initialization more robust, setup param server stuff for camera node

--- nodes/camera_node.py
+++ nodes/camera_node.py
@@ -1,10 +1,17 @@
 #!/usr/bin/env python
 
-import rospy
+import rospy, cv2, atexit, signal
+from sensor_msgs.msg import Image
+from cv_bridge import CvBridge, CvBridgeError
 
 '''
 This module is responsible for interfacing with camera and publishing camera frames.
 '''
+
+CAM_DEVICE_INDEX = 1 # TODO: put this in .yaml file; this holds device index for camera sensor.
+SHOW_CAM = True 	 # TODO: pub this in a param file (same as above)
+CAM_TOPIC = "cam_image"
+CV_BRIDGE_IMAGE_ENCODING = "bgr8"
 
 class camera_node(object):
 
@@ -12,8 +19,71 @@ class camera_node(object):
 		'''
 		Camera node constructor
 		'''
-		rospy.init_node("camera_node")
-		pass
+
+		rospy.init_node("camera_node")		# Register this as ROS node named 'camera_node'
+
+		self.cap = None						# Will store opencv capture device
+		self.bridge = CvBridge()			# Bridge used to convert CV images into ROS Image messages
+
+		self._initialize_camera()								# initialize camera
+		atexit.register(self._exit_handler)						# Register function to be called upon exit
+		signal.signal(signal.SIGINT, self._signal_handler)		# Register ctrl-c signal handler
+
+		self.cap_pub = rospy.Publisher(cam_topic, Image, queue_size = 10)	# Create publisher for camera images
+
+		
+
+	def _initialize_camera(self):
+		'''
+		This function is called to initialize the camera sensor.
+		This function in a helper function for the camera node class.
+		'''
+		self.cap = cv2.VideoCapture(CAM_DEVICE_INDEX)
+		
+	def transmit_video(self):
+		'''
+		This function runs until ROS is shutdown or process is killed.  Continuously captures
+		frames and publishes them over ROS topic.
+		'''
+		rate = rospy.Rate(50) # create a 10Hz rate
+
+		while not rospy.is_shutdown():
+			ret, frame = self.cap.read()	# Grab current frame from webcam
+
+			# Only show cv2 image if show cam parameter is true
+			if (SHOW_CAM):
+				cv2.imshow("frame", frame)
+				if cv2.waitKey(1) & 0xFF == ord('q'):
+					break
+			# publish frame as ROS Image message using cv_bridge
+			try:
+				self.cap_pub.publish(self.bridge.cv2_to_imgmsg(frame, CV_BRIDGE_IMAGE_ENCODING))
+			except CvBridgeError, e:
+				# TODO: log error
+				print(e)
+
+			rate.sleep()
+
+	def _signal_handler(self, signal, frame):
+		'''
+		This function is called when ctr-c signal is received.
+		'''
+		print("received signal: " + str(signal))
+		exit()
+
+
+	def _exit_handler(self):
+		'''
+		This function is called on exit.
+		'''
+		# Release camera and destroy all cv2 windows on exit.
+		try:
+			self.cap.release()
+			cv2.destroyAllWindows()
+		except:
+			pass
+
 
 if __name__ == "__main__":
 	cam_node = camera_node()
+	cam_node.transmit_video()
