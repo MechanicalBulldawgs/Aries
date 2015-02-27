58bc17a3aa2eb1b9a77cbb2f22e2b58b1ae23197
Alex Lalejini <amlalejini@gmail.com>
2015-02-27 17:54:29 -0600

    Added lib_robotis library for dynamixel.

--- nodes/lidar_pivot_controller.py
+++ nodes/lidar_pivot_controller.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 
 import rospy
+from  lib_robotis import USB2Dynamixel_Device, Robotis_Servo
+from math import *
 
 '''
 This module is responsible for interfacing with lidar pivot motor.
@@ -13,7 +15,15 @@ class lidar_pivot_controller(object):
 		lidar pivot controller constructor
 		'''
 		rospy.init_node("lidar_pivot_controller")
-		pass
+		self.dyn = USB2Dynamixel_Device(dev_name = "/dev/ttyUSB0", baudrate = 1000000)
+		self.servo = Robotis_Servo(self.dyn, 1)
+
+		cmd = None
+		while (True):
+			print("Current Angle: " + str(degrees(self.servo.read_angle())))
+			cmd = float(raw_input("> "))
+			if cmd == "q": break
+			self.servo.move_angle(radians(cmd))
 
 if __name__ == "__main__":
 	controller = lidar_pivot_controller()
