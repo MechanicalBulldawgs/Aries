74beae50ac7436373c17d0356da34b7d8222ad62
Alex Lalejini <amlalejini@gmail.com>
2015-03-05 22:27:38 -0600

    Added Josh's lidar_pivot_controller and servo_config updates

--- nodes/lidar_pivot_controller.py
+++ nodes/lidar_pivot_controller.py
@@ -3,6 +3,7 @@
 import rospy
 from  lib_robotis import USB2Dynamixel_Device, Robotis_Servo
 from math import *
+from std_msgs.msg import Float32
 
 '''
 This module is responsible for interfacing with lidar pivot motor.
@@ -10,20 +11,34 @@ This module is responsible for interfacing with lidar pivot motor.
 
 class lidar_pivot_controller(object):
 
-	def __init__(self):
-		'''
-		lidar pivot controller constructor
-		'''
-		rospy.init_node("lidar_pivot_controller")
-		self.dyn = USB2Dynamixel_Device(dev_name = "/dev/ttyUSB0", baudrate = 1000000)
-		self.servo = Robotis_Servo(self.dyn, 1)
+    def __init__(self):
+        '''
+        lidar pivot controller constructor
+        '''
+        self.angle = 0
+        #Creates the ROS node.
+        rospy.init_node("lidar_pivot_controller")
 
-		cmd = None
-		while (True):
-			print("Current Angle: " + str(degrees(self.servo.read_angle())))
-			cmd = float(raw_input("> "))
-			if cmd == "q": break
-			self.servo.move_angle(radians(cmd))
+        #Servo Motor Setup
+        self.dyn = USB2Dynamixel_Device(dev_name = "/dev/ttyUSB0", baudrate = 1000000)
+        self.servo = Robotis_Servo(self.dyn, 1)
+
+        #Inits the LIDAR Subscriber
+        rospy.Subscriber("lidar_pivot_angle",Float32,self.angle_callback)
+
+    def angle_callback(self,angle):
+        self.angle = angle.data
+
+
+    def run(self):
+        #Runs while shut down message is not recieved.
+        while not rospy.is_shutdown():
+
+            #Sends command to move Dynamixel to absolute position. 
+            self.servo.move_angle(radians(self.angle))
+
+            rospy.sleep(0.5)    #Keeps ROS from crashing
 
 if __name__ == "__main__":
-	controller = lidar_pivot_controller()
+    controller = lidar_pivot_controller()
+    controller.run()
