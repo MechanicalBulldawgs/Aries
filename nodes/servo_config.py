bff94928f9bc7f66715d43b37bfee6d55768814e
Alex Lalejini <amlalejini@gmail.com>
2015-03-19 20:39:42 -0500

    Implemented IR distance interrupter arduino code

--- nodes/servo_config.py
+++ nodes/servo_config.py
@@ -7,7 +7,7 @@ servo_param = {
         'rad_per_enc': math.radians(300.0) / 1024.0, 
         'max_ang': math.radians(100),
         'min_ang': math.radians(-10),
-        'flipped': False,
-        'max_speed': math.radians(130)
+        'flipped': True,
+        'max_speed': math.radians(130) / 2.0
 	}
 }
