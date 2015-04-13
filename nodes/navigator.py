40d7255b8d2721806a0da6eb8d60758505aa7f8d
Alex Lalejini <amlalejini@gmail.com>
2015-04-12 20:38:19 -0500

    Added goal publisher test node

---
+++ nodes/navigator.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+
+
+'''
+This module is responsible for sending Twist commands to robot.
+Input: Goal, Obstacle Avoidance Scan
+Output: Twist commands
+'''
+
+class ReactiveNavitor(object):
+
+	def __init__(self):
+		'''
+		'''
+		pass
+
+if __name__ == "__main__":
+	navigator = ReactiveNavitor()
+	
