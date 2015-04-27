084f45b4db92731480fe6fcb3e99267ab3fe99c3
amlalejini <amlalejini@gmail.com>
2015-04-26 20:02:33 -0500

    Added functionality to motor_director.py

--- nodes/motor_director.py
+++ nodes/motor_director.py
@@ -3,15 +3,25 @@
 """This node interfaces with an Arduino to control motors """
 
 import rospy, serial, atexit
+from threading import Lock
+from collections import deque
 from geometry_msgs.msg import Twist
+from std_msgs.msg import Int16
+
+###############
+# Constants (TODO: parameter files)
+HOPPER_CONTROL = 2 # PWM channel for hopper control
+CONVEYOR_SPIN = 3  # PWM channel for conveyor spin motor
+CONVEYOR_TILT = 4  # PWM channel for conveyor tilt motor
+###############
 
 class motor_director(object):
-	'''
-	'''
+	
 	def __init__(self):
 		
 		rospy.init_node('motor_director')
-
+		self.cmd_queue = deque() # python's 'deck' data structure.  Used to store queue of commands received from callbacks to be handled.
+		self.queue_lock = Lock()
 		"""Attempt to get parameters from the ROS server and use them to initialize the list 
 			of touch sensors and the connection to the Arduino"""
 
@@ -20,24 +30,49 @@ class motor_director(object):
 		self.arduino = serial.Serial(port, 9600, timeout = 1)
 		print("Connected to Arduino on port: " + str(port))
 		rospy.Subscriber("cmd_vel", Twist, self.cmd_vel_cb)
+		rospy.Subscriber("hopper_control", Int16, self.dump_callback)
+		rospy.Subscriber("collector_spin_control", Int16, self.collector_spin_callback)
+		rospy.Subscriber("collector_tilt_control", Int16, self.collector_tilt_callback)
+
 		atexit.register(self._exit_handler)
 
-		
+	def run(self):
+		'''
+		This function processes cmds in cmd queue
+		'''
+		rate = rospy.Rate(200)
+		while not rospy.is_shutdown():
+			with self.queue_lock:
+				if len(self.cmd_queue) > 0:
+					print("cmd_queue length: " + str(len(self.cmd_queue)))
+					cmd = self.cmd_queue.pop()
+					self.arduino.write(str(cmd))
+			rate.sleep()
+
 	def cmd_vel_cb(self, data):
-		self.arduino.write('X:'+str(data.linear.x)+'\n')
-		self.arduino.write('Z:'+str(data.angular.z)+'\n')
+		'''
+		Command velocity callback.  
+		'''
+		linear_cmd = "X:" + str(data.linear.x) + "\n"
+		angular_cmd = "Z:" + str(data.angular.z) + "\n"
+		with self.queue_lock:
+			self.cmd_queue.appendleft(linear_cmd)
+			self.cmd_queue.appendleft(angular_cmd)
 
-	'''
-	TODO: add subscribers and callbacks for the motors
-	'''
+	def dump_callback(self, data):
+		cmd = str(HOPPER_CONTROL) + ":" + str(data.data) + "\n"
+		with self.queue_lock:
+			self.cmd_queue.appendleft(cmd)
 
-	'''
-	Formats for messages to the arduino
+	def collector_spin_callback(self, data):
+		cmd = str(CONVEYOR_SPIN_CONTROL) + ":" + str(data.data) + "\n"
+		with self.queue_lock:
+			self.cmd_queue.appendleft(cmd)
 
-	[0-F]:[150:600]
-	X:[linear velocity]
-	Z:[angular velocity]
-	'''
+	def collector_tilt_callback(self, data):
+		cmd = str(CONVEYOR_TILT_CONTROL) + ":" + str(data.data) + "\n"
+		with self.queue_lock:
+			self.cmd_queue.appendleft(cmd)
 	
 	def _exit_handler(self):
 		self.arduino.close()
@@ -48,4 +83,4 @@ if __name__ == "__main__":
 	except rospy.ROSInterruptException as er:
 		rospy.logerr(str(er))
 	else:
-		rospy.spin()
+		ser.run()
